9a19431dd7725186cb15bf56373d0b562a57f08a
Rafael Araujo <cruzarujo486@gmail.com>
2026-07-17 16:35:31 -0300

    logica do menu hamburguer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from "./menu/menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('project');
+  ismenu = signal<boolean>(false);
+  screenWidth = signal<number>(typeof window !== 'undefined' ? window.innerWidth : 1200);
+
+  @HostListener("window:resize")
+  onResize() {
+    if (typeof window !== 'undefined') {
+      this.screenWidth.set(window.innerWidth);
+      if (this.screenWidth() < 768) {
+        this.ismenu.set(true);
+      }
+    }
+  }
+
+  changeMenu(ismenu: boolean): void {
+     this.ismenu.set(ismenu);
+  }
 }
--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+export interface MenuItem {
+  routerLink?: string;
+  click?: string;
+  icon: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {}
+export class Menu {
+  ismenu = input.required<boolean>();
+  changeMenu = output<boolean>();
+
+  items: MenuItem[] = [
+    {
+      click: "fisico",
+      icon: "fa fa-person-skating",
+      label: "Físico",
+    }
+  ];
+
+  onItemClick(item: MenuItem) {
+    if (item.click) {
+      console.log('Abrir modal:', item.click);
+      // Aqui você poderá adicionar a lógica para abrir seu modal
+    }
+  }
+
+  toggleCollaps(): void {
+    this.changeMenu.emit(!this.ismenu());
+  }
+
+  closeSidenav(): void {
+    this.changeMenu.emit(true);
+  }
+}
